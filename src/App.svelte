37761d817b8f3463b3a362f76489b19b35c0e5c2
luispfcanales <luispfcanales@gmail.com>
2021-07-22 21:20:35 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Router, Route, Link } from "svelte-routing"
+</script>
+
+<Router basepath="/svelte-deploy">
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="about">About</Link>
+	</nav>
+
+	<main>
+		<Route path="/">
+			<h3>Home Route</h3>
+		</Route>
+
+		<Route path="about">
+			<h3>About Route</h3>
+		</Route>
+		<Route path="*">
+			<h3>error</h3>
+		</Route>
+	</main>
+</Router>
+
+<style>
+	h3 {
+		color:darkslateblue;
+	}
+</style>
